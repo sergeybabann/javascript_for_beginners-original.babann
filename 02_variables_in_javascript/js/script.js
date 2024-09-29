2490f7b038e7e42ca1a7eaab08f0a1e1ba62a3b4
sergeybabann <sergeybabann@yandex.ru>
2024-09-29 19:30:30 +0300

    add files

--- 02_variables_in_javascript/js/script.js
+++ 02_variables_in_javascript/js/script.js
@@ -1,7 +1,29 @@
-let message = 'Привет'
+// console.log(message)
 
-console.log(message)
+// let message = 'Привет'
 
-message = 'Пока'
+// console.log(message)
 
-console.log(message)
+// message = 'Пока'
+
+// console.log(message)
+
+
+// const name = 'Александр'
+// const age = 28
+// const isDeveloper = true
+
+
+// const name = 'Александр'
+// const admin = 'name'
+
+// console.log(admin)
+
+// const age = 18
+// const age = 20
+
+'use strict'
+
+const age = 10
+
+console.log(age)
