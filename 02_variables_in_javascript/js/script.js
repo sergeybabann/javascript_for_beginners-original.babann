180c1b2b35143995c83b0a15fc311cb46b2e6e42
sergeybabann <sergeybabann@yandex.ru>
2024-09-29 11:02:37 +0300

    add files

---
+++ 02_variables_in_javascript/js/script.js
@@ -0,0 +1,40 @@
+// const message3 = 'goodby'
+// message3 = 'hello'
+// console.log(message3)
+
+// const age1 = 18
+// const age1 = 20 
+
+'use strict'
+
+// age_2 = 10
+// console.log(age_2)
+
+// const const = 1
+// const for = 2
+// const this = 3
+
+
+
+
+const name1 = 'Александр'
+const admin = name1
+console.log(admin)
+
+
+const name = 'Александр', 
+age = 28, 
+isDeveloper = true;
+console.log(name, age,isDeveloper)
+
+
+console.log(message1)
+var message1 = 'hello'
+
+
+let message
+message = 'привет'
+console.log(message)
+
+message = 'пока'
+console.log(message)
---
+++ 03_data_types/js/script.js
@@ -0,0 +1,41 @@
+const greeting = 'привет'
+const name = 'Александр'
+const goodby = 'пока'
+
+const message = `${greeting}, ${name}!`
+console.log(message)
+
+
+const a = 5 
+const b = 10
+const sum = `сумма a и b равна ${a + b}`
+const sum_2 = 'сумма a и b равна ' + (a + b)
+console.log(sum)
+console.log(sum_2)
+
+
+const message_2 = `
+это
+длинное 
+сообщение
+на 
+несколько
+строк
+`
+console.log(message_2)
+
+
+const posX = 50
+const posY = -100
+const posZ = 1.55
+const point = (posX + posY + posZ) * 2 / posX
+console.log(point)
+
+console.log('слово' / 100)
+console.log( 100 / 0)
+console.log( -100 / 0)
+
+
+
+
+
